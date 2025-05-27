91119e43a46784d8b671099420360691cc8e8b17
Yeison <ycaroorrego@gmail.com>
2025-05-26 19:58:34 -0500

    Update CabeceraCV.jsx

--- src/Components/CabeceraCV.jsx
+++ src/Components/CabeceraCV.jsx
@@ -1,10 +1,19 @@
+import "./Cabecera.css";
+
+const user = {
+  nombre:"Brayan Andrey Maldonado Aguirre",
+  cargo:"Desarrollador Back-End",
+  numero:"+57 3197967953",
+  correo:"brayanmald10@gmail.com"
+}
+
 export default function CabeceraCV() {
   return (
     <header>
-      <h1>Yeison Caro</h1>
-      <h2>Desarrollador Back-End </h2>
-      <h3>+57 3014420725</h3>
-      <h4>ycaroorrego@gmail.com</h4>
+      <h1>{user.nombre}</h1>
+      <h2>{user.cargo}</h2>
+      <h3>{user.numero}</h3>
+      <h4>{user.correo}</h4>
     </header>
   );
 }
