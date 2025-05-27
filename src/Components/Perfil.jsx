2b0f833547296391f5b9d7833ce97ef9a1e7d908
Yeison <ycaroorrego@gmail.com>
2025-05-26 19:58:54 -0500

    Update Perfil.jsx

--- src/Components/Perfil.jsx
+++ src/Components/Perfil.jsx
@@ -1,10 +1,15 @@
+
+const user = {
+  perfil:"Perfil",
+  informacion:"Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología. Mis habilidades son tales como manejo de ciertos lenguajes,Python,Java y soy puntual entregando trabajos ",
+}
+
 export default function Perfil() {
   return (
     <section>
-      <h3>Perfil</h3>
+      <h3>{user.perfil}</h3>
       <p>
-        Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología.
-        Mis habilidades son tales como manejo de ciertos lenguajes,Python,Java y soy puntual entregando trabajos 
+        {user.informacion}
       </p>
     </section>
   );
