27c3af9a946457ebe95ab5a7dea9c4265b670885
Yeison <ycaroorrego@gmail.com>
2025-05-28 12:53:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import CabeceraCV from "./Components/CabeceraCV.jsx";
+import Perfil from "./Components/Perfil.jsx";
+import Educacion from "./Components/Educacion.jsx";
+import Experiencia from "./Components/Experiencia.jsx";
+import StackTecnologias from "./Components/StackTecnologias.jsx";
+
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <CabeceraCV />
+      <Perfil />
+      <Educacion />
+      <Experiencia />
+      <StackTecnologias/>
+    </div>
   );
 }
 
-export default App
+export default App;
