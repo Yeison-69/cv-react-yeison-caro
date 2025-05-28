747d168d2afcdc7b8f0daeadee0148a57d9d24e4
Yeison <ycaroorrego@gmail.com>
2025-05-28 13:29:58 -0500

    Update Perfil.jsx

--- src/Components/Perfil.jsx
+++ src/Components/Perfil.jsx
@@ -1,7 +1,7 @@
 
 const user = {
   perfil:"Perfil",
-  informacion:"Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología. Mis habilidades son tales como manejo de ciertos lenguajes,Python,Java y soy puntual entregando trabajos ",
+  informacion:"Tecnólogo en Análisis y Desarrollo de Software con experiencia en desarrollo web y móvil. Apasionado por la tecnología, con habilidades sólidas en programación, diseño de interfaces y gestión de proyectos. Me destaco por ser responsable, puntual en la entrega de trabajos y con capacidad para adaptarme a nuevos entornos tecnológicos. Con conocimientos en múltiples lenguajes como Python y Java, y dominio en frameworks modernos como React y Node.js. ",
 }
 
 export default function Perfil() {
