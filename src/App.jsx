2f3114943a86c855b6c84fe6a45161afe064cb23
Yeison <ycaroorrego@gmail.com>
2025-06-04 18:35:44 +0000

    refactor: extracción de datos personales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
-import CabeceraCV from "./Components/CabeceraCV.jsx";
-import Perfil from "./Components/Perfil.jsx";
-import Educacion from "./Components/Educacion.jsx";
-import Experiencia from "./Components/Experiencia.jsx";
-import StackTecnologias from "./Components/StackTecnologias.jsx";
+// src/App.jsx
+import React from "react";
+import {
+  cabecera,
+  perfil,
+  educacion,
+  experiencia,
+  stackTecnologias,
+  proyectos,
+  habilidades
+} from "./data/cvData";
 
+import CabeceraCV from "./components/CabeceraCV";
+import Perfil from "./components/Perfil";
+import Educacion from "./components/Educacion";
+import Experiencia from "./components/Experiencia";
+import StackTecnologias from "./components/StackTecnologias";
+import Proyectos from "./components/Proyectos";
+import Habilidades from "./components/Habilidades";
 
 function App() {
   return (
-    <div>
-      <CabeceraCV />
-      <Perfil />
-      <Educacion />
-      <Experiencia />
-      <StackTecnologias/>
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      {/* Cabecera */}
+      <CabeceraCV
+        nombre={cabecera.nombre}
+        profesion={cabecera.profesion}
+      />
+
+      {/* Perfil */}
+      <Perfil texto={perfil.texto} />
+
+      {/* Educación */}
+      <Educacion estudios={educacion} />
+
+      {/* Experiencia */}
+      <Experiencia trabajos={experiencia} />
+
+      {/* Stack de Tecnologías */}
+      <StackTecnologias tecnologias={stackTecnologias} />
+
+      {/* Proyectos (nuevo componente) */}
+      <Proyectos proyectos={proyectos} />
+
+      {/* Habilidades (nuevo componente) */}
+      <Habilidades habilidades={habilidades} />
     </div>
   );
 }
